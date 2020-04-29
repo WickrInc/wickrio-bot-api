4666009a33045e85ba34119ed2b2d090d5803a46
pwcushman <pcushman@wickr.com>
2020-04-29 17:01:18 -0400

    Paul fixmessageparse (#16)
    * Fix the message parsing, was failing
    
    * up the version number
    
    * Remove logging

--- WickrIOBot.js
+++ WickrIOBot.js
@@ -80,13 +80,11 @@ class WickrIOBot {
                     } else {
                         administrators = tokens.ADMINISTRATORS.value;
                     }
-
                     administrators = administrators.split(',');
 
                     // Make sure there are no white spaces on the whitelisted users
                     for(var i = 0; i < administrators.length; i++){
                         var administrator = administrators[i].trim();
-                        console.log("administrator:" + administrator);
                         var admin = myLocalAdmins.addAdmin(administrator);
                     }
                 }
@@ -341,7 +339,8 @@ class WickrIOBot {
           'isVoiceMemo': isVoiceMemo,
           'voiceMemoDuration': voiceMemoDuration,
           'convotype': convoType,
-          'isAdmin' : isAdmin
+          'isAdmin' : isAdmin,
+          'msgtype' : 'file'
         };
       } else {
         var parsedObj = {
@@ -351,19 +350,52 @@ class WickrIOBot {
           'userEmail': sender,
           'isVoiceMemo': isVoiceMemo,
           'convotype': convoType,
-          'isAdmin' : isAdmin
+          'isAdmin' : isAdmin,
+          'msgtype' : 'file'
         };
       }
       return parsedObj;
+    } else if (message.location) {
+      var parsedObj = {
+        'latitude': message.location.latitude,
+        'longitude': message.location.longitude,
+        'vgroupid': vGroupID,
+        'userEmail': sender,
+        'convotype': convoType,
+        'isAdmin' : isAdmin,
+        'msgtype' : 'location'
+      };
+      return parsedObj;
+    } else if (message.call) {
+      var parsedObj = {
+        'status': message.call.status,
+        'vgroupid': vGroupID,
+        'userEmail': sender,
+        'convotype': convoType,
+        'isAdmin' : isAdmin,
+        'msgtype' : 'call'
+      };
+      return parsedObj;
+    } else if (message.keyverify) {
+      var parsedObj = {
+        'vgroupid': vGroupID,
+        'userEmail': sender,
+        'convotype': convoType,
+        'isAdmin' : isAdmin,
+        'msgtype' : 'keyverify'
+      };
+      return parsedObj;
+    } else if (message.control) {
+      return;
+    } else if (message.message === undefined) {
+      return;
     }
+
     var request = message.message;
     var command = '',
       argument = '';
-    if (message.control)
-      return;
-    else
-      //This doesn't capture @ mentions
-      var parsedData = request.match(/(\/[a-zA-Z]+)([\s\S]*)$/);
+    //This doesn't capture @ mentions
+    var parsedData = request.match(/(\/[a-zA-Z]+)([\s\S]*)$/);
     if (parsedData !== null) {
       command = parsedData[1];
       if (parsedData[2] !== '') {
@@ -384,7 +416,8 @@ class WickrIOBot {
       'vgroupid': vGroupID,
       'userEmail': sender,
       'convotype': convoType,
-      'isAdmin' : isAdmin
+      'isAdmin' : isAdmin,
+      'msgtype' : 'message'
     };
 
     return parsedObj;
